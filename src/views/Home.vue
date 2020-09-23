dfaeb6031da029ec11f83ac98ed790f7a775da28
florianwoelki <florianwoelki@gmx.de>
2020-09-23 17:01:35 +0200

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-full flex items-center justify-center">
+    <h1 class="text-6xl font-bold text-red-600">Hello World</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class Home extends Vue {}
+</script>
