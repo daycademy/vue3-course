f7964b0bec4ed2ee7d634270bf62ed8748dc9b1a
florianwoelki <florianwoelki@gmx.de>
2020-10-10 08:44:43 +0200

    initial commit for lesson 4

---
+++ src/components/InputField.vue
@@ -0,0 +1,18 @@
+<template>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,25 @@
 <template>
-  <div class="h-full flex items-center justify-center">
-    <Button @click="showNotification">
-      Klick mich
-    </Button>
-  </div>
+  <div class="flex items-center justify-center h-full">
+    <div class="px-12 py-10 bg-white rounded-lg shadow-2xl">
+      <InputField v-model="myInput" />
 
-  <transition
-    enter-active-class="transition-all ease-in-out duration-300"
-    leave-active-class="transition-all ease-in-out duration-300"
-    enter-from-class="opacity-0"
-    leave-to-class="opacity-0"
-  >
-    <div
-      v-if="isNotificationVisible"
-      class="absolute mt-4 ml-4 px-4 py-2 top-0 left-0 bg-white rounded-lg shadow-2xl text-sm"
-    >
-      Button wurde geklickt!
+      <p class="mt-4 text-gray-700">
+        Input: {{ myInput }}
+      </p>
     </div>
-  </transition>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import Button from '@/components/Button.vue';
+import InputField from '@/components/InputField.vue';
 
 @Options({
   components: {
-    Button,
+    InputField,
   },
 })
 export default class Home extends Vue {
-  private isNotificationVisible = false;
-
-  private showNotification(payload: { duration: number }): void {
-    this.isNotificationVisible = true;
-
-    setTimeout(() => {
-      this.isNotificationVisible = false;
-    }, payload.duration);
-  }
+  myInput = '';
 }
 </script>
